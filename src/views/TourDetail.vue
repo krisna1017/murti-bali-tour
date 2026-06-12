bbfd8a8089929b92a6bf7174e16f3cb7c01a0b90
krisna1017 <kadekpermana05@gmail.com>
2026-06-12 17:16:03 +0800

    update style responsive mobile

--- src/views/TourDetail.vue
+++ src/views/TourDetail.vue
@@ -3,7 +3,7 @@
     <div v-if="tour" class="min-h-screen px-4 bg-white pt-24 pb-16 font-poppins text-slate-800 text-left">
       <div class="max-w-6xl mx-auto lg:col-span-2 space-y-3">
 
-        <div class="bg-transparent rounded-3xl lg:flex items-center justify-between">
+        <div class="bg-transparent rounded-3xl md:flex items-center justify-between">
           <h1 class="text-lg md:text-3xl font-regular tracking-tight text-sky-800 mb-1">
             {{ tour.name }}
           </h1>
@@ -56,10 +56,10 @@
         </div>
 
         <!-- DESKTOP GRID -->
-        <div class="hidden md:grid grid-cols-4 grid-rows-2 gap-3 h-150">
+        <div class="hidden md:grid grid-cols-12 grid-rows-2 h-150">
 
           <div 
-            class="row-span-2 col-span-2 cursor-pointer overflow-hidden rounded-lg"
+            class="row-span-2 col-span-6 cursor-pointer overflow-hidden rounded-lg mr-3"
             @click="openDesktopGallery(0)"
           >
             <img 
@@ -70,7 +70,7 @@
           </div>
 
           <div 
-            class="row-span-1 col-span-1 cursor-pointer overflow-hidden rounded-lg"
+            class="row-span-1 col-span-3 cursor-pointer overflow-hidden rounded-lg mb-3"
             @click="openDesktopGallery(1)"
           >
             <img 
@@ -81,7 +81,7 @@
           </div>
 
           <div 
-            class="row-span-1 col-span-1 cursor-pointer overflow-hidden rounded-lg"
+            class="row-span-1 col-span-3 cursor-pointer overflow-hidden rounded-lg mb-3 ml-3"
             @click="openDesktopGallery(2)"
           >
             <img 
@@ -92,7 +92,7 @@
           </div>
 
           <div 
-            class="row-span-2 col-span-1 cursor-pointer overflow-hidden rounded-lg"
+            class="row-span-2 col-span-3 cursor-pointer overflow-hidden rounded-lg"
             @click="openDesktopGallery(3)"
           >
             <img 
@@ -103,7 +103,7 @@
           </div>
 
           <div 
-            class="row-span-2 col-span-1 cursor-pointer overflow-hidden rounded-lg"
+            class="row-span-2 col-span-3 cursor-pointer overflow-hidden rounded-lg ml-3"
             @click="openDesktopGallery(4)"
           >
             <img 
@@ -154,18 +154,18 @@
         </div>
     
 
-        <div class="mx-auto lg:flex">
-          <div class="max-w-full md:max-w-2/3 grid grid-cols-1 gap-2 items-start lg:text-2xl">
+        <div class="mx-auto block lg:grid lg:grid-cols-12 gap-12 md:gap-6 items-start">
+          <div class="lg:col-span-8 space-y-5 lg:text-2xl">
             <a 
               :href="whatsappUrl"
               target="_blank"
-              class="w-full bg-sky-500 hover:bg-sky-600 text-white font-bold py-3.5 px-4 rounded-2xl transition-all shadow-md shadow-emerald-500/20 text-md flex items-center justify-center gap-2 cursor-pointer mt-6 md:hidden"
+              class="w-full bg-sky-500 hover:bg-sky-600 text-white font-bold py-3.5 px-4 rounded-2xl transition-all shadow-md shadow-emerald-500/20 text-md flex items-center justify-center cursor-pointer mt-6 md:hidden"
             >
               Book This Package
             </a>
 
             <div class="">
-              <p class="text-slate-600 text-sm md:text-base leading-relaxed whitespace-pre-line">
+              <p class="text-slate-600 text-sm md:text-base leading-relaxed whitespace-pre-line text-justify md:mt-6">
                 {{ tour.description }}
               </p>
             </div>
@@ -262,84 +262,87 @@
 
           </div>
 
-          <div class=" max-w-full md:max-w-1/3 lg:col-span-1">
-              <div class="bg-white border border-slate-100 rounded-3xl p-6 shadow-md shadow-slate-300">
-                <div class="flex items-baseline gap-2 mt-1 mb-6">
-                  <span class="text-3xl font-thin">From</span>
-                  <span class="text-3xl font-regular text-sky-500">
-                    {{ tour.price_list[0]?.name }}
-                  </span>
-                </div>
-    
-                <div class="my-4 space-y-3 text-xs text-black">
-                  <div class="flex items-center gap-2">
-                    <i class="pi pi-calendar text-xl"></i>
-                    <span class="text-md">Book now pay later</span>
+          <div class="lg:col-span-4 mt-6 self-start">
+              <div class="md:overflow-y-auto md:pr-2 md:max-h-200">
+                <div class="bg-white border border-slate-100 rounded-3xl p-6 shadow-md shadow-slate-300">
+                  <div class="flex items-baseline gap-2 mt-1 mb-6">
+                    <span class="text-3xl md:text-2xl font-thin">From</span>
+                    <span class="text-3xl md:text-2xl font-regular text-sky-500">
+                      {{ tour.price_list[0]?.name }}
+                    </span>
                   </div>
-                  <div class="flex items-center gap-2">
-                    <i class="pi pi-thumbs-up text-xl"></i>
-                    <span class="text-md">The best services guarantee</span>
+      
+                  <div class="my-4 space-y-3 text-xs text-black">
+                    <div class="flex items-center gap-2">
+                      <i class="pi pi-calendar text-xl"></i>
+                      <span class="text-md">Book now pay later</span>
+                    </div>
+                    <div class="flex items-center gap-2">
+                      <i class="pi pi-thumbs-up text-xl"></i>
+                      <span class="text-md">The best services guarantee</span>
+                    </div>
                   </div>
-                </div>
-    
-                <a 
-                  :href="whatsappUrl"
-                  target="_blank"
-                  class="w-full bg-sky-500 hover:bg-sky-600 text-white font-bold py-3.5 px-4 rounded-2xl transition-all shadow-md shadow-emerald-500/20 text-sm flex items-center justify-center gap-2 cursor-pointer mt-6"
-                >
-                  Book Right Now !
-                </a>
-              </div>
-
-              <div v-if="otherTours.length > 0" class="col-span-1 mt-12 border-t border-slate-200 pt-10">
-                <h2 class="text-xl font-bold text-slate-900 border-l-4 border-sky-500 pl-3 mb-6">
-                  Rekomendasi Paket Wisata Lainnya
-                </h2>
-                
-                <div class="grid grid-cols-1 gap-6">
-                  <RouterLink 
-                    v-for="item in otherTours" 
-                    :key="item.slug"
-                    :to="`/packages/${currentCategory.slug}/${item.slug}`" 
-                    class="bg-white rounded-3xl overflow-hidden shadow-sm border border-slate-100 hover:shadow-md transition-all group"
+      
+                  <a 
+                    :href="whatsappUrl"
+                    target="_blank"
+                    class="w-full bg-sky-500 hover:bg-sky-600 text-white font-bold py-3.5 px-4 rounded-2xl transition-all shadow-md shadow-emerald-500/20 text-sm flex items-center justify-center gap-2 cursor-pointer mt-6"
                   >
-                    <div class="overflow-hidden aspect-6/3 w-full bg-gray-100">
-                      <img
-                        :src="item.image"
-                        :alt="item.name"
-                        class="w-full h-full object-cover group-hover:scale-110 transition duration-500"
-                      />
-                    </div>
-          
-                    <div class="p-4 text-left flex-1 flex flex-col justify-between">
-                      <div>
-                        <h3 class="text-base font-bold text-sky-900 leading-snug group-hover:text-sky-500 transition-colors">
-                          {{ item.name }}
-                        </h3>
-          
-                        <div class="flex items-center gap-1 mt-1">
-                          <span class="text-yellow-400 text-sm">★★★★★</span>
-                          <span class="text-gray-500 text-xs font-medium">({{ item.rating || '5.0' }} rating)</span>
-                        </div>
-                      </div>
-          
-                      <div class="flex items-center justify-between mt-5 pt-3 border-t border-slate-100">
-                        <div class="flex items-baseline gap-0.5">
-                          <span class="text-lg font-extrabold text-gray-800">
-                            {{ item.price_list[0]?.name }}
-                          </span>
-                          <span class="text-gray-400 text-xs">/pax</span>
-                        </div>
-          
-                        <span class="text-xs font-bold text-gray-600 group-hover:text-sky-500 transition-colors flex items-center gap-1">
-                          Read More ➔
-                        </span>
-                      </div>
-          
-                    </div>
-                  </RouterLink>
+                    Book Right Now !
+                  </a>
                 </div>
-
+    
+                <div v-if="otherTours.length > 0" class="col-span-1 mt-12 border-t border-slate-200 pt-10">
+                  <h2 class="text-xl font-bold text-slate-900 border-l-4 border-sky-500 pl-3 mb-6">
+                    Rekomendasi Paket Wisata Lainnya
+                  </h2>
+                  
+                  <div class="grid grid-cols-1 gap-6">
+                    <RouterLink 
+                      v-for="item in otherTours" 
+                      :key="item.slug"
+                      :to="`/packages/${currentCategory.slug}/${item.slug}`" 
+                      class="bg-white rounded-3xl overflow-hidden shadow-sm border border-slate-100 hover:shadow-md transition-all group"
+                    >
+                      <div class="overflow-hidden aspect-6/3 w-full bg-gray-100">
+                        <img
+                          :src="item.image"
+                          :alt="item.name"
+                          class="w-full h-full object-cover group-hover:scale-110 transition duration-500"
+                        />
+                      </div>
+            
+                      <div class="p-4 text-left flex-1 flex flex-col justify-between">
+                        <div>
+                          <h3 class="text-base font-bold text-sky-900 leading-snug group-hover:text-sky-500 transition-colors">
+                            {{ item.name }}
+                          </h3>
+            
+                          <div class="flex items-center gap-1 mt-1">
+                            <span class="text-yellow-400 text-sm">★★★★★</span>
+                            <span class="text-gray-500 text-xs font-medium">({{ item.rating || '5.0' }} rating)</span>
+                          </div>
+                        </div>
+            
+                        <div class="flex items-center justify-between mt-5 pt-3 border-t border-slate-100">
+                          <div class="flex items-baseline gap-0.5">
+                            <span class="text-lg font-extrabold text-gray-800">
+                              {{ item.price_list[0]?.name }}
+                            </span>
+                            <span class="text-gray-400 text-xs">/pax</span>
+                          </div>
+            
+                          <span class="text-xs font-bold text-gray-600 group-hover:text-sky-500 transition-colors flex items-center gap-1">
+                            Read More ➔
+                          </span>
+                        </div>
+            
+                      </div>
+                    </RouterLink>
+                  </div>
+    
+                </div>
+    
               </div>
           </div>
         </div>
